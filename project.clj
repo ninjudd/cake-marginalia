d635704077cf0aa184f6a803369dff72547eaa50
zkim <zachary.kim@gmail.com>
2011-06-26 18:15:21 +0000

    Updated README, bump to 0.6.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cake-marginalia "1.0.0-SNAPSHOT"
+(defproject cake-marginalia "0.6.0"
   :description "Cake plugin for Marginalia."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [marginalia "0.6.0"]]
